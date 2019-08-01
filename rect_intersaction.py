f714b269731135074233ab6877a26750980c670b
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2019-08-01 14:36:01 +0400

    Update rect_intersaction.py

--- rect_intersaction.py
+++ rect_intersaction.py
@@ -1,7 +1,10 @@
+import json
 from collections import namedtuple
 
 Line = namedtuple("Line", ["xmin", "xmax"],  rename=False)
 # we will need this namedtuple in the class
+
+# for modifing parametrs go to line 150
 
 
 class Rectangle:
@@ -44,7 +47,6 @@ class Rectangle:
         # functions returns expanded line we will use it in expander function
         return [min(line_1.xmin, line_2.xmin), max(line_1.xmax, line_2.xmax)]
 
-    # @staticmethod
     def line_croper(self, line_1, line_2):
         # functions returns croped line we will use it in croper function
         # assumes that  line_1 is important
@@ -101,9 +103,9 @@ class Rectangle:
             new_min_y, new_max_y = self.line_croper(
                 self.line_y, other.line_y)
 
-            area_1 = (new_max_x - new_max_x) * \
+            area_1 = (new_max_x - new_min_x) * \
                      (other.ymax - other.ymin)
-            area_2 = (new_max_y - new_max_y) * \
+            area_2 = (new_max_y - new_min_y) * \
                      (other.xmax - other.xmin)
 
             if area_1 < area_2:
@@ -114,13 +116,6 @@ class Rectangle:
                 return ("Second rectangle's new cordinates are: \n" +
                         "   xmin, ymin: " + str(new_min_x) + "  " + str(other.xmin) +
                         "\n   xmax, ymax: " + str(new_max_x) + "  " + str(other.xmax))
-
-        # else:
-        #     new_min_x, new_max_x = self.line_croper(other.line_y, self.line_x)
-        #     new_min_y, new_max_y = self.line_croper(other.line_y, self.line_y)
-
-        #     self.line_x = Line(new_min_x, new_max_x)
-        #     self.line_y = Line(new_min_y, new_max_y)
 
     def expander(self, other):
         # this functions expands less important rectangle so that imported
@@ -135,6 +130,7 @@ class Rectangle:
                     "   xmin, ymin: " + str(new_min_x) + "  " + str(new_min_y) +
                     "\n   xmax, ymax: " + str(new_max_x) + "  " + str(new_max_y))
 
+        # depending of rects importance it changes one of them
         else:
             self.line_x = Line(new_min_x, new_max_x)
             self.line_y = Line(new_min_y, new_max_y)
@@ -143,7 +139,8 @@ class Rectangle:
                     "\n   xmax, ymax: " + str(new_max_x) + "  " + str(new_max_y))
 
     def case_classifier(self, other):
-        # function checks how big is overlaping part  and decide to crop or expand
+        # function checks how big is overlaping part and decide to crop or expand
+        # also this fucntions runs function coresponding to classified case
         little_rect = min(self.area, other.area)
         overlap_percentage = self.calculate_overlap_area(other) / little_rect
         if overlap_percentage < self.threshold:
@@ -152,18 +149,31 @@ class Rectangle:
 
 
 # threshold helps to understand what to use (cropper or expander)
-threshold = 0.99
-importance_first = 5
+threshold = 0.5
+importance_first = 8
 importance_second = 6
-re_1 = Rectangle(1, 1, 7, 5, importance_first, threshold)
-re_2 = Rectangle(2, 2, 8, 5, importance_second, threshold)
+# re_1 = Rectangle(2, 5, 6, 8, importance_first, threshold)
+# re_2 = Rectangle(4, 2, 8, 6, importance_second, threshold)
 
+re_1 = Rectangle(2, 5, 6, 8, importance_first, threshold)
+re_2 = Rectangle(4, 2, 5, 6, importance_second, threshold)
+
+print("First rectangle's cordinates are: \n" +
+      "   xmin, ymin: " + str(re_1.xmin) + "  " + str(re_1.ymin) +
+      "\n   xmax, ymax: " + str(re_1.xmax) + "  " + str(re_1.ymax))
+print("Second rectangle's cordinates are: \n" +
+      "   xmin, ymin: " + str(re_2.xmin) + "  " + str(re_2.ymin) +
+      "\n   xmax, ymax: " + str(re_2.xmax) + "  " + str(re_2.ymax))
+
+print()
 
 print("First rectangle's area is: " + str(re_1.area))
 print("Second rectangle's area is: " + str(re_2.area))
 print()
 print("Overlap area is: " + str(re_1.calculate_overlap_area(re_2)))
 print(re_1.check_if_nested(re_2))
+print()
+
 if not (re_1.check_if_nested(re_2) == "None of rectangles are nested"):
     print("No need to change anything")
 else:
@@ -171,3 +181,18 @@ else:
         print(re_1.case_classifier(re_2))
     else:
         print(re_2.case_classifier(re_1))
+
+
+# print(vars(re_2))
+
+# converting to json
+
+Square_to_json = namedtuple("Square", 'xmin ymin xmax ymax')
+
+first_square = Square_to_json(re_1.xmin, re_1.ymin, re_1.xmax, re_1.ymax)
+second_square = Square_to_json(re_2.xmin, re_2.ymin, re_2.xmax, re_2.ymax)
+
+first = json.dumps(first_square._asdict(), indent=4)
+second = json.dumps(second_square._asdict(), indent=4)
+
+# print(first)
