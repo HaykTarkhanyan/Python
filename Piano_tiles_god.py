80bf7f84c2c144accab1d7787a762958f6b3ab00
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2019-12-11 13:13:34 +0400

    added some comments and explained its just for fun

--- Piano_tiles_god.py
+++ Piano_tiles_god.py
@@ -1,6 +1,11 @@
+"""
+    Just fun code which uses pyautigui library to make moves
+    in the game piano tiles, where you have to press to black tiles
+"""
 import pyautogui as pg
 
 first_cord =  [198,624]
+
 second_cord = [290,624]
 third_cord =  [389,624]
 forth_cord =  [481,624]
@@ -9,14 +14,10 @@ pixel_color = [51,69,69]
 
 pg.moveTo(second_cord)
 
-# def check_if_same(arr1,arr2):
-#     for i,j in zip(arr1,arr2):
-#         if i!=j:
-#             return False
-#     return True
-
 def check_if_near(arr1,arr2):
-    
+  """
+    Looks on the pixel values and calculates there euclidian distance
+  """
   dist = 0
   for i in range(len(arr1)):
     dist += (arr1[i]-arr2[i])**2
@@ -26,7 +27,6 @@ def check_if_near(arr1,arr2):
 
 
 tiles = [first_cord, second_cord, third_cord, forth_cord]
-
 
 for j in range(700):
     for i in range(len(tiles)):
